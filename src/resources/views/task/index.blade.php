2b21fe7529b4d805eeb7a02b1905f14693e5858f
kt5007 <kenjin.0546@gmail.com>
2023-02-04 16:29:17 +0900

    create header

--- src/resources/views/task/index.blade.php
+++ src/resources/views/task/index.blade.php
@@ -6,14 +6,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/header.css'])
 
     <title>Hello, world!</title>
 </head>
 <body>
     @include('layouts.header')
     <h1>Hello, world!</h1>
-    <button type="button" class="btn btn-link">Link</button>
+    <span class="badge rounded-pill bg-primary">Primary</span>
 </body>
 
 </html>
