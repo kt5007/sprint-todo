dcf299f376657409a652fb44174447783d8b65d8
kt5007 <kenjin.0546@gmail.com>
2023-02-04 14:40:32 +0900

    install bootstrap5

--- src/resources/views/task/index.blade.php
+++ src/resources/views/task/index.blade.php
@@ -1,1 +1,19 @@
-hello
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <title>Hello, world!</title>
+</head>
+<body>
+    @include('layouts.header')
+    <h1>Hello, world!</h1>
+    <button type="button" class="btn btn-link">Link</button>
+</body>
+
+</html>
