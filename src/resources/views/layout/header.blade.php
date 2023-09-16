62d78675c5d14fa131d320933a403d54b20cedca
kt5007 <kenjin.0546@gmail.com>
2023-09-16 19:36:34 +0900

    styles

--- src/resources/views/layout/header.blade.php
+++ src/resources/views/layout/header.blade.php
@@ -1,5 +1,5 @@
-<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom bg-light">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom bg-dark">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
         <svg class="bi me-2" width="40" height="32">
             <use xlink:href="#bootstrap"></use>
         </svg>
@@ -7,10 +7,10 @@
     </a>
 
     <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">ホーム</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">タスク</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">空き時間</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">スプリント</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">メンバー</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">ホーム</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">タスク</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">空き時間</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">スプリント</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-white">メンバー</a></li>
     </ul>
 </header>
--- src/resources/views/user/index.blade.php
+++ src/resources/views/user/index.blade.php
@@ -1,22 +1,18 @@
-<!-- resources/views/home.blade.php -->
-<!-- Specify that we want to extend the index file -->
 @extends('layout.master')
-<!-- Set the title content to "Home" -->
+
 @section('title', 'Home')
-<!-- Set the "content" section, which will replace "@yield('content')" in the index file we're extending -->
 
 @section('content')
-    <!-- Add User Registration Button -->
-    <div>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#userRegistrationModal">
-            ユーザー登録
-        </button>
-    </div>
     <div class="card p-2">
         <div class="card-body">
-            <table class="table">
-                <thead>
-                    <tr>
+            <div class="mb-3">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#userRegistrationModal">
+                    ユーザー登録
+                </button>
+            </div>
+            <table class="table table-hover table-sm">
+                <thead class="thead-light">
+                    <tr class="table-secondary">
                         <th scope="col">ID</th>
                         <th scope="col">登録名</th>
                         <th scope="col">操作</th>
