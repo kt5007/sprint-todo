4fe85e5ae47df1909ef0a8297eb4f5dc8ec908a1
kt5007 <kenjin.0546@gmail.com>
2023-09-16 17:22:58 +0900

    user insert

---
+++ src/app/Services/UserService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use App\Services;
+use Illuminate\Support\Collection;
+use DB;
+use Illuminate\Http\Request;
+
+class UserService
+{
+    // userを1人新規作成
+    public function register(Request $request): void
+    {
+        $request->username;
+        
+    }
+}
