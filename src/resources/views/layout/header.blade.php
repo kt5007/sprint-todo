b83bade5a1717914775cb6797ce38be232726e79
kt5007 <kenjin.0546@gmail.com>
2023-09-16 15:59:14 +0900

    Sample User index view

---
+++ src/resources/views/layout/header.blade.php
@@ -0,0 +1,16 @@
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32">
+            <use xlink:href="#bootstrap"></use>
+        </svg>
+        <span class="fs-4">Simple header</span>
+    </a>
+
+    <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">SPRINT-TODO</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">タスク</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">空き時間</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">スプリント</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">メンバー</a></li>
+    </ul>
+</header>
---
+++ src/resources/views/layout/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <title>@yield('title') - Hotel Manager</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    @include('layout.header')
+    <main>@yield('content')</main>
+</body>
+
+</html>
