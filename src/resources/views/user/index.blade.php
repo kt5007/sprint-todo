3e9a37b617a1301f3a1dfe39b165196fbf2e4a16
kt5007 <kenjin.0546@gmail.com>
2023-09-16 16:16:25 +0900

    user index

--- src/resources/views/user/index.blade.php
+++ src/resources/views/user/index.blade.php
@@ -5,39 +5,40 @@
 @section('title', 'Home')
 <!-- Set the "content" section, which will replace "@yield('content')" in the index file we're extending -->
 @section('content')
-<div class="jumbotron text-light" style="background-image: url('https://source.unsplash.com/1600x900/?nature')">
-    <div class="container">
-        <h1 class="display-3">Reservation management made easy.</h1>
-        <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam in quia natus magnam ducimus quas molestias velit vero maiores. Eaque sunt laudantium voluptas. Fugiat molestiae ipsa delectus iusto vel quod.</p>
-        <a href="/login" class="btn btn-success btn-lg my-2">Sign Up for Access to Thousands of Hotels</a>             
+    <div class="card p-2">
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @for($i = 0; $i < 100; $i++)
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+
+                    @endfor
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-sm-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Convenient</h5>
-                    <p class="card-text">Manage all your hotel reservations in one place</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Best prices</h5>
-                    <p class="card-text">We have special discounts at the best hotels</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Easy to use</h5>
-                    <p class="card-text">Book and manage with the click of a button</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
